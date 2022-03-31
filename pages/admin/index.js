0e710c9294a649ff0ec2f971e8953692a5c41d43
Thor-Erik Støvland <thor-erik.stovland@oo.no>
2022-03-31 15:27:48 +0200

    admin page

---
+++ pages/admin/index.js
@@ -0,0 +1,10 @@
+import Layout from "../../components/layout/Layout";
+import NavMenu from "../../components/nav/NavMenu";
+
+const Admin = () => {
+  return ( <>
+    
+  </> );
+}
+ 
+export default Admin;
---
+++ pages/admin/messages.js
@@ -0,0 +1,15 @@
+import Container from "../../components/layout/Container";
+import MessageItems from "../../components/MessageItems";
+import TravelTips from "../../components/TravelTips";
+
+const Messages = () => {
+  return ( 
+    <Container>
+      <h1>Messages</h1>
+      <MessageItems headings={['Name', 'Subject', 'Date']} />
+      <TravelTips />
+    </Container>
+   );
+}
+ 
+export default Messages;
