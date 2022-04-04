00455b390b5cee60a3b9cb25c8015a6c59a7848f
Thor-Erik Stovland <thorerik88@hotmail.com>
2022-04-04 07:57:15 +0200

    Added viewport to _app

--- components/layout/Head.js
+++ components/layout/Head.js
@@ -3,6 +3,7 @@ import NextHead from 'next/head';
 const Head = ({ title = '' }) => {
     return ( 
         <NextHead>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>
                 Holidaze{title ? ' | ' : ''}
                 {title}
--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,7 @@ const Document = () => {
       <Head>
       <meta charset="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      
+            
       <meta name="robot" content="follow, index" />
       <meta name="description" content="Ditt lokale rørleggerfirma Rør & Service Beitostølen leverer vvs tjenester av høy kvalitet" />
       <meta name="keywords" content="rørlegger, beitostølen, beitostoelen, kvalitet, service, thonny, støvland" />
