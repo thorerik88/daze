b56920e8ecce4c0426e933e3a384e1f8927fe9da
Thor-Erik Stovland <thorerik88@hotmail.com>
2022-04-21 15:44:09 +0200

    added last items to form

--- components/establishments/Reservation.js
+++ components/establishments/Reservation.js
@@ -7,63 +7,65 @@ import Container from "../layout/Container";
 import Button from "../layout/Button";
 import Nationality from './Nationality';
 
+const handleReservation = () => {
+  
+}
+
 const Reservation = () => {
   return (   
-    <>
-      
-      <form className={styles.form} name='contact' type='submit'>
-        <div className={styles.contentWrapper}>
-          <h2>Make a reservation</h2>
-          <div className={styles.inputGroup}>
-            <input type='text' name='name' />
-            <span>Name</span>
-          </div>
-          <div className={styles.inputGroup}>
-            <input type='number' name='phone' />
-            <span>Phone</span>
-          </div>
-          <div className={styles.inputGroups}>
-            <div className={styles.col}>
-              <input type='date' name='checkin' />
-              <span>Checkin</span>
-            </div>
-            <div className={styles.col}>
-              <input type='date' name='checkout' />
-              <span>Checkout</span>
-            </div>
-          </div>
-          <div className={styles.inputGroups}>
-            <div className={styles.qty}>
-              <span>Guests</span>
-              <div className={styles.qtyAction}>
-                <FontAwesomeIcon className={styles.actionButton} icon={faMinus} />
-                <input type='number' name='checkin' value='1' />
-                <FontAwesomeIcon className={styles.actionButton} icon={faPlus} />
-              </div>
-            </div>
-            <div className={styles.qty}>
-              <span>Rooms</span>
-              <div className={styles.qtyAction}>
-                <FontAwesomeIcon className={styles.actionButton} icon={faMinus} />
-                <input type='number' name='checkin' value='1' />
-                <FontAwesomeIcon className={styles.actionButton} icon={faPlus} />
-              </div>
-            </div>
-          </div>
-          <div className={styles.inputGroup}>
-            <Nationality />
-            <span>Nationality</span>
-          </div>
-          <div className={styles.inputGroup}>
-                <div className={styles.checkboxes}>
-                  <input id='newsletter' name='newsletter' type='checkbox' />
-                  <label htmlFor='newsletter'>Newsletter</label>
-                </div>
-              </div>
-          <Button value={'Make Reservation'} buttonType={'submit'}/>
+    <form className={styles.form} name='contact' type='submit'>
+      <div className={styles.contentWrapper}>
+        <h2>Make a reservation</h2>
+        <div className={styles.inputGroup}>
+          <input type='text' name='name' />
+          <span>Name</span>
         </div>
-      </form>
-    </>
+        <div className={styles.inputGroup}>
+          <input type='number' name='phone' />
+          <span>Phone</span>
+        </div>
+        <div className={styles.inputGroups}>
+          <div className={styles.col}>
+            <input type='date' name='checkin' />
+            <span>Checkin</span>
+          </div>
+          <div className={styles.col}>
+            <input type='date' name='checkout' />
+            <span>Checkout</span>
+          </div>
+        </div>
+        <div className={styles.inputGroups}>
+          <div className={styles.qty}>
+            <span>Guests</span>
+            <div className={styles.qtyAction}>
+              <FontAwesomeIcon className={styles.actionButton} icon={faMinus} />
+              <input type='number' name='checkin' value='1' readOnly='defaultValue'/>
+              <FontAwesomeIcon className={styles.actionButton} icon={faPlus} />
+            </div>
+          </div>
+          <div className={styles.qty}>
+            <span>Rooms</span>
+            <div className={styles.qtyAction}>
+              <FontAwesomeIcon className={styles.actionButton} icon={faMinus} />
+              <input type='number' name='checkin' value='1' readOnly='defaultValue'/>
+              <FontAwesomeIcon className={styles.actionButton} icon={faPlus} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.inputGroup}>
+          <Nationality />
+          <span>Nationality</span>
+        </div>
+        <div className={styles.inputGroup}>
+              <div className={styles.checkboxes}>
+                <input id='newsletter' name='newsletter' type='checkbox' />
+                <label htmlFor='newsletter'>Newsletter</label>
+              </div>
+            </div>
+        <Button onClick={handleReservation} value={'Make Reservation'} buttonType={'submit'}/>
+      </div>
+    </form>
+  
   );
 }
  
